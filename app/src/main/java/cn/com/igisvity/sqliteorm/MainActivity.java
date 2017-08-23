799a2d6861b117849645e7ca0263e2a561fa957d
zhouzhou <zhouzhou>
2017-08-23 14:03:54 +0800

    2

--- app/src/main/java/cn/com/igisvity/sqliteorm/MainActivity.java
+++ app/src/main/java/cn/com/igisvity/sqliteorm/MainActivity.java
@@ -3,11 +3,14 @@ package cn.com.igisvity.sqliteorm;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import cn.com.igisvity.sqliteormlib.DBHelper;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DBHelper.initDBHelper("test.db");
     }
 }
