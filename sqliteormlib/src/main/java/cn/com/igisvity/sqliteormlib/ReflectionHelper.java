880f9079c990f4eef6a08e6ef0a25e2af7387dd5
zhouzhou <zhouzhou>
2017-09-07 12:12:02 +0800

    2

--- sqliteormlib/src/main/java/cn/com/igisvity/sqliteormlib/ReflectionHelper.java
+++ sqliteormlib/src/main/java/cn/com/igisvity/sqliteormlib/ReflectionHelper.java
@@ -14,103 +14,6 @@ import java.lang.reflect.Method;
  * @date 2015-9-24 下午4:01:53
  */
 public class ReflectionHelper {
-    /**
-     * 快捷填充对象(标准化的对象)
-     *
-     * @param MethodName
-     * @param o          调用此方法的对象
-     * @param paras      调用的这个方法的参数参数列表
-     */
-    public static void getMethod(String MethodName, Object o, Object[] paras) {
-        Class c[] = null;
-        if (paras != null) {// 存在
-            int len = paras.length;
-            c = new Class[len];
-            for (int i = 0; i < len; ++i) {
-                c[i] = paras[i].getClass();
-            }
-        }
-        try {
-            Method method = null;
-            if (c[0].getSimpleName().equals("Integer")) {
-                method = o.getClass().getDeclaredMethod(MethodName, int.class);
-            } else {
-                method = o.getClass().getDeclaredMethod(MethodName, c);
-            }
-            try {
-                method.invoke(o, paras);// 调用o对象的方法
-            } catch (IllegalAccessException ex) {
-                ex.fillInStackTrace();
-            } catch (IllegalArgumentException ex) {
-                ex.fillInStackTrace();
-            } catch (InvocationTargetException ex) {
-                ex.fillInStackTrace();
-            }
-        } catch (NoSuchMethodException ex) {
-            ex.fillInStackTrace();
-        } catch (SecurityException ex) {
-            ex.fillInStackTrace();
-        }
-
-    }
-
-    public static void getMethod(String MethodName, Object o, Object paras) {
-        Class c = null;
-        if (paras != null) {// 存在
-            c = paras.getClass();
-        }
-        try {
-            Method method = null;
-            if (c.getSimpleName().equals("Integer")) {
-                method = o.getClass().getDeclaredMethod(MethodName, int.class);
-            } else {
-                method = o.getClass().getDeclaredMethod(MethodName, c);
-            }
-            try {
-                method.invoke(o, paras);// 调用o对象的方法
-            } catch (IllegalAccessException ex) {
-                ex.fillInStackTrace();
-            } catch (IllegalArgumentException ex) {
-                ex.fillInStackTrace();
-            } catch (InvocationTargetException ex) {
-                ex.fillInStackTrace();
-            }
-        } catch (NoSuchMethodException ex) {
-            ex.fillInStackTrace();
-        } catch (SecurityException ex) {
-            ex.fillInStackTrace();
-        }
-
-    }
-
-    public static void doMethod(String MethodName, Object o, Object paras) {
-        Class c = null;
-        if (paras != null) {// 存在
-            c = paras.getClass();
-        }
-        try {
-            Method method = null;
-            if (c.getSimpleName().equals("Integer")) {
-                method = o.getClass().getDeclaredMethod(MethodName, int.class);
-            } else {
-                method = o.getClass().getDeclaredMethod(MethodName, c);
-            }
-            try {
-                method.invoke(o, paras);// 调用o对象的方法
-            } catch (IllegalAccessException ex) {
-                ex.fillInStackTrace();
-            } catch (IllegalArgumentException ex) {
-                ex.fillInStackTrace();
-            } catch (InvocationTargetException ex) {
-                ex.fillInStackTrace();
-            }
-        } catch (NoSuchMethodException ex) {
-            ex.fillInStackTrace();
-        } catch (SecurityException ex) {
-            ex.fillInStackTrace();
-        }
-
-    }
 
     public static void doMethod(String MethodName, Object o, String paras, Class parasType) {
         try {
@@ -118,7 +21,7 @@ public class ReflectionHelper {
             try {
                 if (parasType.equals(int.class)) {
                     method.invoke(o, Integer.parseInt(paras));
-                } else if (parasType.equals(Double.class)) {
+                } else if (parasType.equals(double.class)) {
                     method.invoke(o, Double.parseDouble(paras));
                 } else
                     method.invoke(o, paras);// 调用o对象的方法
